5884e68b82717015595f5283380f295da248c277
Brian Moffatt <moffabr1@Brians-MBP.ptd.net>
2016-09-19 08:09:50 -0400

    created the controllers for every route to our api using only the necessary methods

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+
+        return 'I am in Index';
+    }
+
+}
